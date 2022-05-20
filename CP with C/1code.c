4ec580f64186872109d50ea4d859f31ed3d3dd9f
Ripadiu <105770175+Ripadiu@users.noreply.github.com>
2022-05-20 19:57:15 +0600

    Add files via upload

---
+++ CP with C/1code.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+int main()
+{
+    printf("It's feeling really wonderful.\n");
+    return 0;
+}
---
+++ CP with C/2code.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main()
+{
+    int a=10,b=20;
+    int calculation=((a+b)/3)*2;
+    printf("%d\n",calculation);
+    return 0;
+}
